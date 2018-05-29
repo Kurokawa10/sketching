18ae62f1b1bd2090951817bbbeb800290aa6caf2
Roberto <rgarciaggr@gmail.com>
2018-05-29 02:17:33 +0200

    -Funcionalidades principales acabadas    -Falta testing    -Implementar mejoras segun surjan

--- sketching.sql
+++ sketching.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-05-2018 a las 18:09:30
+-- Tiempo de generación: 29-05-2018 a las 02:16:25
 -- Versión del servidor: 10.1.32-MariaDB
 -- Versión de PHP: 7.0.30
 
@@ -75,12 +75,15 @@ CREATE TABLE `galeria` (
 
 INSERT INTO `galeria` (`id`, `nombre`, `descripcion`, `autor`, `fecha_creacion`, `visitas`, `tipo`) VALUES
 (1, 'Prueba', 'Esta galeria esta hardcodeada, es solo de pruebas', 1, '2018-05-20 20:00:00', 1, 5),
-(2, 'Prueba2', 'Esta galeria tambien esta hardcodeada, es solo de pruebas', 1, '2018-05-20 20:00:00', 1, 0),
+(2, 'Prueba2', 'Esta galeria tambien esta hardcodeada, es solo de pruebas', 1, '2018-05-20 20:00:00', 2, 0),
 (3, 'Mas pruebas hardc', 'Esta galeria tambien esta hardcodeada, es solo de pruebas', 1, '2018-05-20 20:00:00', 5, 1),
-(4, 'Otra pruebas hardc', 'Esta galeria tambien esta hardcodeada, es solo de pruebas', 1, '2018-05-20 20:00:00', 0, 5),
+(4, 'Otra pruebas hardc', 'Esta galeria tambien esta hardcodeada, es solo de pruebas', 1, '2018-05-20 20:00:00', 1, 5),
 (5, '5 pruebas hardc', 'Esta galeria tambien esta hardcodeada, es solo de pruebas', 1, '2018-05-20 20:00:00', 2, 3),
-(6, '6 pruebas hardc', 'Esta galeria tambien esta hardcodeada, es solo de pruebas', 1, '2018-05-20 20:00:00', 12, 1),
-(7, '7 prueba', 'Esta galeria tambien esta hardcodeada, es solo de pruebas', 1, '2018-05-20 20:00:00', 179, 0);
+(6, '6 pruebas hardc', 'Esta galeria tambien esta hardcodeada, es solo de pruebas', 1, '2018-05-20 20:00:00', 15, 1),
+(7, '7 prueba', 'Esta galeria tambien esta hardcodeada, es solo de pruebas', 1, '2018-05-20 20:00:00', 193, 0),
+(38, 'Galeria Buena', 'Galeria creada por el autor desde New gallery', 35, '2018-05-29 02:04:39', 1, 1),
+(39, 'Galeria de pruebas', 'Galeria de pruebas', 35, '2018-05-29 02:06:36', 2, 0),
+(40, 'Galeria para mis mejores fans', 'Exclusive content', 35, '2018-05-29 02:09:14', 3, 5);
 
 -- --------------------------------------------------------
 
@@ -116,7 +119,8 @@ CREATE TABLE `subs` (
 
 INSERT INTO `subs` (`id`, `id_user`, `id_autor`, `tipo`) VALUES
 (1, 30, 1, 5),
-(2, 5, 1, 5);
+(2, 5, 1, 5),
+(5, 39, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -147,7 +151,7 @@ INSERT INTO `usuarios` (`id`, `username`, `email`, `nombre`, `apellido`, `birth_
 (32, 'cargonben1', 'carlo64@hotmail.com', 'Carlos', 'CASd', '1987-05-05', 'cargonben1.jpg', ''),
 (34, 'kurokawa10', 'robert_gg@hotmail.com', 'Roberto García Gordi', 'Gordillo', '1987-05-05', 'kurokawa10.jpg', ''),
 (35, 'kurokawa11', 'robert5_gg@hotmail.co', 'Roberto García Gordi', 'Gordillo', '1987-05-01', 'kurokawa11.jpg', ''),
-(39, 'rgarciag10', 'robert5_gg@hotmail.com1', 'Roberto García Gordi', 'Gordillo', '1999-05-02', 'rgarciag10.png', ''),
+(39, 'rgarciag11', 'robert5_gg@hotmail.com2', 'Roberto García Gordi', 'Gordillo', '1999-05-02', 'rgarciag11.jpg', 'Fanatico del dibujo, ayudame a ayudar a ayudate.'),
 (40, 'Hasam', 'jsmjsmjsm17@gmail.com', 'Jorge', 'Sánchez Muñoz', '1997-05-06', '', ''),
 (41, 'PichaBrava69', 'ainfanter01@gmail.com', 'PichaBrava69', 'PichaBrava69', '1993-12-29', 'PichaBrava69.jpg', '');
 
@@ -205,7 +209,7 @@ ALTER TABLE `acceso`
 -- AUTO_INCREMENT de la tabla `galeria`
 --
 ALTER TABLE `galeria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT de la tabla `pagos`
@@ -217,7 +221,7 @@ ALTER TABLE `pagos`
 -- AUTO_INCREMENT de la tabla `subs`
 --
 ALTER TABLE `subs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
